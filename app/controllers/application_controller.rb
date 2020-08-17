a2bdd84eacdeec61fb57c17b21f122f68c3415ac
neivyx <janeivyhilario@gmail.com>
2020-08-17 11:09:34 -0400

    +added helpers methdos current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,5 +19,11 @@ class ApplicationController < Sinatra::Base
     end 
   end 
 
+  helpers do 
+
+    def current_user
+     @current_user ||= User.find_by(id:session[:user_id])
+    end 
+
  
 end
