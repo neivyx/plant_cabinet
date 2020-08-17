04ef60ffd46e3c1406795d58f697d31643a09755
neivyx <janeivyhilario@gmail.com>
2020-08-17 11:27:36 -0400

    post /plants Plant.new(params)

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,10 +4,26 @@ class PlantsController < ApplicationController
     @plants = Plant.all
     erb :"/plants/index.html"
   end
- #CREATE 
+  
   get '/plants/new' do 
     redirect_if_not_logged_in
     erb :"/plants/new.html"
   end 
+
+
+  post '/plants' do  
+    redirect_if_not_logged_in
+    if params[:name] != ""
+      
+      @plant = Plant.create(name: params[:name], user_id: current_user.id, location: params[:location], light_requirement: params[:light_requirment], watering: params[:watering], description: params[:description])
+    
+      redirect "/plants/#{@plant.id}"
+    else 
+    
+      redirect '/plants/new.html'
+    end 
+  end
+
+
    
 end
