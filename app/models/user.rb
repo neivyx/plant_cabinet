d9084f82a571c8a01d9cebb82ab3cdfb185b2352
neivyx <janeivyhilario@gmail.com>
2020-08-25 14:04:50 -0400

    +validatations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
-    has_secure_password
-
+    has_secure_password 
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true 
     has_many :plants
 
 end
